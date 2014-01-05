3f11f874c15db0db371dd556dd78c6a8a9dd730e
Sami Samhuri <sami@samhuri.net>
2014-01-05 12:13:51 -0800

    iOS v3.0.9

--- TapForTap-iOS-3.0.8/Readme.md
+++
@@ -1,5 +0,0 @@
-# TapForTap iOS SDK
-
-For documentation visit:
-
-https://tapfortap.com/doc/ios/integration
---
+++ TapForTap-iOS-3.0.9/example-app/example-app/TFTAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  TFTAppDelegate.h
+//  example-app
+//
+//  Created by Phil Laird on 2013-07-18.
+//  Copyright (c) 2013 Tap for Tap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TFTViewController;
+
+@interface TFTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) TFTViewController *viewController;
+
+@end
